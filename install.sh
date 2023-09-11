776d02facf35171450cbe889bd31f6e759193ce8
zhangrengang <zhangrenang@ori-gene.cn>
2023-09-11 18:52:42 +0800

    raise error when chromosomes are not recognized

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # install dependency
+[ ! -s REPcluster/setup.py ] && \
+	echo 'Submodule REPcluster do not exit. Please use `git clone --recurse-submodules`' && \
+	exit
 (cd REPcluster && python3 setup.py install)
 
 # install
